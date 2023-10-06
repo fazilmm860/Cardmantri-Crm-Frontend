8d0e03a6ba2b102490ecb1da1bc75a078dd42eac
Mohamed Fazil M <fazilmm860@gmail.com>
2023-10-06 11:38:43 +0530

    edit in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -23,7 +23,7 @@ const Navbar = () => {
     const { logindata, setLoginData } = useContext(LoginContext);
 
     return (
-        <Disclosure as="nav" className="bg-gray-800">
+        <Disclosure as="nav" className="bg-yellow-500 text-white  font-bold">
             {({ open }) => (
                 <>
                     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
